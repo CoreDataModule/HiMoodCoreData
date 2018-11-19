f08d296696c35614314e781ac1defd29ef900622
HaviLee <Havi.X.Li.-ND@disney.com>
2018-11-19 09:25:11 +0800

    merge feature/havi/demo1 into developer

---
+++ CoreDemo/View Controllers/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  CoreDemo
+//
+//  Created by Li, Havi X. -ND on 2018/11/18.
+//  Copyright © 2018 Li, Havi X. -ND. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class RootViewController: UIViewController {
+	
+	var manageObjectContext: NSManagedObjectContext!
+	override func viewDidLoad() {
+		self.view.backgroundColor = UIColor.red
+	}
+	
+}
